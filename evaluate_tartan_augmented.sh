e837f047bc5719695abdbf4e78e32246a75bb6ae
jessezhaoxizhang <jessezhaoxizhang@gmail.com>
2025-08-27 16:13:29 +0800

    o

--- evaluate_tartan_augmented.sh
+++ evaluate_tartan_augmented.sh
@@ -2,4 +2,4 @@
 python evaluate_tartan_augmented.py --weights=/path/to/your/pth/file.pth --path=/path/for/storage --ablation_name=csvo --save_trajectory 
 
 # For DPVO evaluation (RGB only)
-python evaluate_tartan_augmented.py --weights=/path/to/your/pth/file.pth --path=/path/for/storage --ablation_name=csvo --save_trajectory
+python evaluate_tartan_augmented.py --weights=/path/to/your/pth/file.pth --path=/path/for/storage --ablation_name=dpvo --save_trajectory
