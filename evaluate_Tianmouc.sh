3a8509262f1f95e550bd8a7043ca36b8f68ee463
jessezhaoxizhang <jessezhaoxizhang@gmail.com>
2025-08-27 15:02:18 +0800

    with '#' will be ignored, and an empty message aborts the commit.

---
+++ evaluate_Tianmouc.sh
@@ -0,0 +1,5 @@
+# For CSVO evaluation
+python evaluate_Tianmouc.py --weights=/path/to/your/pth/file.pth --path=/path/for/storage --ablation_name=csvo --tianmouc_data_dir=/path/to/dir/containing/tianmouc/data --save_trajectory 
+
+# For DPVO evaluation
+python evaluate_Tianmouc.py --weights=/path/to/your/pth/file.pth --path=/path/for/storage --ablation_name=dpvo --tianmouc_data_dir=/path/to/dir/containing/tianmouc/data --save_trajectory 
